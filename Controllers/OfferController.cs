3d62831dd40667da41a87a67ff89322590e6fe61
Bortoli94 <gbortoli.rosario@hotmail.com>
2022-10-06 21:15:43 -0300

    add list user and offer

--- Controllers/OfferController.cs
+++ Controllers/OfferController.cs
@@ -6,6 +6,17 @@ namespace TPI_Programación3.Controllers
 {
     public class OfferController : Controller
     {
+
+        List<Offer> _offers = new()
+        {
+
+            new Offer(1, "Licuadora", "Es literalmente una licuadora", "Electrodoméstico", "https://url24.top/vJGZp", "alejo@gmail.com", "Batidora"),
+            new Offer(2, "Tostadora", "Sirve para tostar pan", "Electrodoméstico", "https://url24.top/AvyPV", "gaston_elcapo@gmail.com", "Tostadora"),
+            new Offer(3, "Auto", "Corsita 2009", "Vehiculo", "https://url24.top/hqPKD", "elmassi@gmail.com", "Auto"),
+            new Offer(4, "Casa", "Casa dos pisos", "Inmueble", "https://url24.top/lPhra", "milton_tucson_tuki@gmail.com", "Vivienda"),
+            new Offer(5, "Figurita", "La figurita de Messi", "Mundial", "https://url24.top/TvJrw", "pedrito@gmail.com", "Mundial"),
+        };
+
         // GET: OfferController
         public ActionResult Index()
         {
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,7 +5,18 @@ using TPI_Programación3.Entities;
 namespace TPI_Programación3.Controllers
 {
     public class UserController : Controller
+
+
     {
+        List<User> _user = new()
+        {
+            new User(1,"Alejo", "alejo@gmail.com", "123", "Common"),
+            new User(2,"Gaston", "gaston_elcapo@gmail.com", "abc", "Common"),
+            new User(3,"Maxi", "elmassi@gmail.com", "789", "Common"),
+            new User(4,"Milton", "milton_tucson_tuki@gmail.com", "qwe", "Administrator"),
+            new User(5,"Pedro", "pedrito@gmail.com", "cxz", "Administrator"),
+        };
+
         // GET: UserController
         public ActionResult Index()
         {
